b0036ecba142835bf2ff6893d5fb3ed7376ae004
Sora Kanosue <sorakanosue@berkeley.edu>
2023-11-01 17:43:21 -0700

    Added instructions

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,2 +1,11 @@
-public interface  Instruction {
+import lombok.Builder;
+
+@lombok.Value
+@Builder
+public class Instruction {
+    Register rs1;
+    Register rs2;
+    Register rd;
+    Value imm;
+    Opcode opcode;
 }
